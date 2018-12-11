7d8dd0c614aba61da8b5d80a7708a1c67b95580f
ina5 <inapanova5@gmail.com>
2018-12-11 15:29:25 +0200

    fixed - handlebars!!!

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { join } from 'path';
 import { AppModule } from './app.module';
-import 'reflect-metadata';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
