1f46892c13866ef1cfedcb33ad02d0e1690299ec
ina5 <inapanova5@gmail.com>
2019-01-03 18:41:10 +0200

    delete node modules from repo

--- node_modules/nest-access-control/decorators/use-roles.decorator.d.ts
+++
@@ -1,6 +0,0 @@
-import { Role } from '../role.interface';
-/**
- * Define an access information required for this route.
- * Notic that all Roles must be satisfied/Passed
- */
-export declare const UseRoles: (...roles: Role[]) => (target: object, key?: any, descriptor?: any) => any;
